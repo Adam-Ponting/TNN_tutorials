3e74a529e9e42ce44cc0bc9bd7e24dc9322136b3
Adam Ponting <adam.ponting@gmail.com>
2019-11-05 19:40:17 +0000

    lesson 9 complete

--- vue_js2_tutorial/app.js
+++ vue_js2_tutorial/app.js
@@ -1,7 +1,29 @@
 new Vue({
     el: '#vue-app', // element to control
     data: { // data holds key: value pairs
-       name: '',
-       age: ''
+        age: 20,
+        a: 0,
+        b: 0
     },
+    methods: {
+        // addToA(){
+        //     console.log('addToA')
+        //     return this.a + this.age
+        // },
+        // addToB(){
+        //     console.log('addToB')
+        //     return this.b
+        //     // return this.b + this.age
+        // }
+    },
+    computed: {
+        addToA(){
+            console.log('addToA')
+            return this.a + this.age
+        },
+        addToB(){
+            console.log('addToB')
+            return this.b + this.age
+        }
+    }
 })
