1df23fa04c5804d704f11cedf39a857567d1d04e
Adam Ponting <adam.ponting@gmail.com>
2019-11-05 20:49:55 +0000

    lesson 13 complete

--- vue_js2_tutorial/app.js
+++ vue_js2_tutorial/app.js
@@ -1,11 +1,19 @@
-new Vue({
-    el: '#vue-app', // element to control
-    data: { // data holds key: value pairs
-        characters: ['Mario', 'Luigi', 'Yoshi', 'Bowser'],
-        ninjas: [
-            { name:'Ryu', age: 25 },
-            { name:'Yoshi', age: 35 },
-            { name:'Ken', age: 55 }
-        ]
-    }
+new Vue ({
+    el: '#vue-app',
+    data: {
+        health: 100,
+        ended: false
+    },
+    methods: {
+        punch() {
+            this.health -= 10
+            if (this.health === 0) {
+                this.ended = true
+            }
+        },
+        restart() {
+            this.health = 100
+            this.ended = false
+        }
+    },
 })
---
+++ vue_js2_tutorial/tut12/app.js
@@ -0,0 +1,11 @@
+new Vue({
+    el: '#vue-app', // element to control
+    data: { // data holds key: value pairs
+        characters: ['Mario', 'Luigi', 'Yoshi', 'Bowser'],
+        ninjas: [
+            { name:'Ryu', age: 25 },
+            { name:'Yoshi', age: 35 },
+            { name:'Ken', age: 55 }
+        ]
+    }
+})
