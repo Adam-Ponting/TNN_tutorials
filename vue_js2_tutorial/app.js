85b2be0db418aa1ac56e6af086415d30f4b3c6bf
Adam Ponting <adam.ponting@gmail.com>
2019-11-05 18:32:13 +0000

    lesson 8 complete

--- vue_js2_tutorial/app.js
+++ vue_js2_tutorial/app.js
@@ -1,15 +1,7 @@
 new Vue({
     el: '#vue-app', // element to control
     data: { // data holds key: value pairs
-       
+       name: '',
+       age: ''
     },
-    methods: {
-        logName(){
-            console.log('You entered your name')
-        },
-        logAge(){
-            console.log('You entered your age')
-        }
-       
-    }
 })
