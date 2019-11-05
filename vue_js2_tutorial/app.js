ad9bcaeccb2f221022888bb0043a96bbaa701882
Adam Ponting <adam.ponting@gmail.com>
2019-11-05 18:28:56 +0000

    lesson 7 complete

--- vue_js2_tutorial/app.js
+++ vue_js2_tutorial/app.js
@@ -1,27 +1,15 @@
 new Vue({
     el: '#vue-app', // element to control
     data: { // data holds key: value pairs
-        age: 38,
-        x: 0,
-        y:0
+       
     },
     methods: {
-        add(increment){
-            this.age += increment
+        logName(){
+            console.log('You entered your name')
         },
-        subtract(decrement){
-            this.age -= decrement
-        },
-        greet(time) { // return object to be shown in the DOM
-            // receives params
-            return `Good ${time}, ${this.name} `
-        },
-        updateXY(e){ // event gets passed by default from eventlistener
-            this.x = e.offsetX;
-            this.y = e.offsetY;
-        },
-        click() {
-            alert('You clicked me')
+        logAge(){
+            console.log('You entered your age')
         }
+       
     }
 })
