cb73386930199136f2bcfd36042c349c489dc7e8
Adam Ponting <adam.ponting@gmail.com>
2019-11-05 17:13:32 +0000

    lesson 3 complete

--- vue_js2_tutorial/app.js
+++ vue_js2_tutorial/app.js
@@ -1,6 +1,14 @@
 new Vue({
     el: '#vue-app', // element to control
-    data: { // data holds key:value pairs
-        name: 'Adam'
+    data: { // data holds key: value pairs
+        name: 'Adam',
+        eh: 'dads',
+        job: 'unemployed'
+    },
+    methods: {
+        greet(time) { // return object to be shown in the DOM
+            // receives params
+            return `Good ${time}, ${this.name} `
+        }
     }
 })
