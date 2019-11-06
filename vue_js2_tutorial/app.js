87750da505c3d3bcd4b32b3fbd6801d2cc81c24d
Adam Ponting <adam.ponting@gmail.com>
2019-11-06 08:23:29 +0000

    lesson 14 complete

--- vue_js2_tutorial/app.js
+++ vue_js2_tutorial/app.js
@@ -1,4 +1,33 @@
-new Vue ({
-    el: '#vue-app',
- 
+// create two instances of vue to control different area of the webpage
+// the variable names (one/two) can allow each instance to reference the other
+var one = new Vue ({
+    el: '#vue-app-one',
+    data: {
+        title: 'Vue app one'
+    },
+    computed: {
+        greeting() {
+            return 'Hello from app one'
+        }
+    }
 })
+
+var two = new Vue ({
+    el: '#vue-app-two',
+    data: {
+        title: 'Vue app two'
+    },
+    methods: {
+        changeTitle() {
+            one.title = 'Title is changed'
+        }
+    },
+    computed:{
+        greeting() {
+            return 'Yo dudes, app two here'
+        }
+    }
+})
+
+// interact with vue instance from outside
+two.title = 'Changed from outside'
