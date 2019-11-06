dc5c7b139531f70e9d9632d4eb806682f5472944
Adam Ponting <adam.ponting@gmail.com>
2019-11-06 08:44:01 +0000

    lesson 16 complete

--- vue_js2_tutorial/app.js
+++ vue_js2_tutorial/app.js
@@ -1,49 +1,17 @@
-// creating a component:
-
-Vue.component('greeting', { // 'component name', object
-    template: '<p>Hello there, I\'m {{ name }}. <button @click="changeName">change name</button></p>',
-    data() { // data is a function in components so each time it's references it's unicue to that instance
-        return {
-            name: 'Yoshi'
-        }  
-    },
-    methods: { // method will only change data in the component where its clicked
-        changeName(){
-            this.name = 'Mario'
-        }
-    }
-})
 
 // create two instances of vue to control different area of the webpage
 // the variable names (one/two) can allow each instance to reference the other
 var one = new Vue ({
-    el: '#vue-app-one',
+    el: '#vue-app',
     data: {
-        title: 'Vue app one'
-    },
-    computed: {
-        greet() {
-            return 'Hello from app one'
-        }
-    }
-})
-
-var two = new Vue ({
-    el: '#vue-app-two',
-    data: {
-        title: 'Vue app two'
-    },
+        output: 'your fave is'
+   },
     methods: {
-        changeTitle() {
-            one.title = 'Title is changed'
-        }
-    },
-    computed:{
-        greet() {
-            return 'Yo dudes, app two here'
+        readRefs() {
+            // access ref with .$refs.name
+            console.log(this.$refs.test.textContent)
+            console.log(this.$refs.inputRef.value)
+            this.output = this.$refs.inputRef.value
         }
     }
 })
-
-// interact with vue instance from outside
-two.title = 'Changed from outside'
