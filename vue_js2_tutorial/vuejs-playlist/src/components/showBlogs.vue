8a082b28cd99f95f20a0f9e29ffc69d314a18768
Adam Ponting <adam.ponting@gmail.com>
2019-11-11 13:32:56 +0000

    lesson 36 complete

--- vue_js2_tutorial/vuejs-playlist/src/components/showBlogs.vue
+++ vue_js2_tutorial/vuejs-playlist/src/components/showBlogs.vue
@@ -1,9 +1,10 @@
 <template>
   <!-- v-theme is a custom directive -->
   <!-- :column is the argument with the directive -->
-  <div v-theme:column="'narrow'" id="show-blogs">
+  <div v-theme:column="'wide'" id="show-blogs">
     <h1>All Blog Articles</h1>
-    <div v-for="(blog, index) in blogs" :key="index" class="single-blog">
+    <input type="text" name id v-model="search" placeholder="search box" />
+    <div v-for="(blog, index) in filteredBlogs" :key="index" class="single-blog">
       <h2 v-rainbow>{{ blog.title | to-uppercase }}</h2>
       <article>{{ blog.body | snippet}}</article>
     </div>
@@ -16,13 +17,21 @@ import axios from "axios";
 export default {
   data() {
     return {
-      blogs: []
+      blogs: [],
+      search: ""
     };
   },
   created() {
     axios.get("https://jsonplaceholder.typicode.com/posts").then(response => {
       this.blogs = response.data.slice(0, 10); // get the first 10 responses
     });
+  },
+  computed: {
+    filteredBlogs() {
+      return this.blogs.filter(blog => {
+        return blog.title.match(this.search); // searches for the search string in the blog title
+      });
+    }
   }
 };
 </script>
