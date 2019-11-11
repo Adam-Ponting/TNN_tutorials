70ad7f6459ba21285a0647905debe6e53d067b4b
Adam Ponting <adam.ponting@gmail.com>
2019-11-11 11:46:59 +0000

    lesson 28 complete

--- vue_js2_tutorial/vuejs-playlist/src/App.vue
+++ vue_js2_tutorial/vuejs-playlist/src/App.vue
@@ -1,35 +1,21 @@
 <template>
   <div>
-    <!-- keep-alive keeps prevent data/component being destroyed when toggleing -->
-    <keep-alive>
-      <!-- component will render the component defined by is="" -->
-      <component :is="component"></component>
-    </keep-alive>
-    <button @click="component = 'form-one'">Show form one</button>
-    <button @click="component = 'form-two'">Show form two</button>
+    <add-blog></add-blog>
   </div>
 </template>
 
 <script>
-// Imports
-import FormOne from "@/components/FormOne.vue";
-import FormTwo from "@/components/FormTwo.vue";
+import addBlog from "@/components/addBlog.vue";
 export default {
   components: {
-    "form-one": FormOne,
-    "form-two": FormTwo
-  },
-  data() {
-    return {
-      component: "form-one"
-    };
+    "add-blog": addBlog
   }
 };
 </script>
 
-<style>
+<style scoped>
 body {
   margin: 0;
-  font-family: "Nunito SemiBold";
+  font-family: "Nunito Semibold";
 }
 </style>
