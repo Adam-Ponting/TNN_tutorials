3d8efd5e727d3bcd0b1a17726e02e470d1780d67
Adam Ponting <adam.ponting@gmail.com>
2019-11-11 13:18:18 +0000

    lesson 34 complete

--- vue_js2_tutorial/vuejs-playlist/src/components/showBlogs.vue
+++ vue_js2_tutorial/vuejs-playlist/src/components/showBlogs.vue
@@ -1,8 +1,10 @@
 <template>
-  <div id="show-blogs">
+  <!-- v-theme is a custom directive -->
+  <!-- :column is the argument with the directive -->
+  <div v-theme:column="'narrow'" id="show-blogs">
     <h1>All Blog Articles</h1>
     <div v-for="(blog, index) in blogs" :key="index" class="single-blog">
-      <h2>{{ blog.title }}</h2>
+      <h2 v-rainbow>{{ blog.title }}</h2>
       <article>{{ blog.body }}</article>
     </div>
   </div>
@@ -19,7 +21,7 @@ export default {
   },
   created() {
     axios.get("https://jsonplaceholder.typicode.com/posts").then(response => {
-      this.blogs = response.data.slice(0, 10);
+      this.blogs = response.data.slice(0, 10); // get the first 10 responses
     });
   }
 };
--- vue_js2_tutorial/vuejs-playlist/src/main.js
+++ vue_js2_tutorial/vuejs-playlist/src/main.js
@@ -8,6 +8,29 @@ import App from './App.vue'
 
 export const eventBus = new Vue() // create event bus, to be imported as import { eventBus } from 'location'
 
+// custom directives 
+Vue.directive('rainbow', {
+  // eslint-disable-next-line no-unused-vars
+  bind(el, binding, vnode){
+    el.style.color = '#' + Math.random().toString().slice(2,8)
+  }
+})
+
+Vue.directive('theme', {
+  // eslint-disable-next-line no-unused-vars
+  bind(el, binding, vnode){
+    if (binding.value === 'wide') {
+      el.style.maxWidth = '1200px'
+    } else if (binding.value === 'narrow') {
+      el.style.maxWidth = '500px'
+    }
+    if (binding.arg === 'column') {
+      el.style.background = '#ddd'
+      el.style.padding = '20px'
+    }
+  }
+})
+
 Vue.config.productionTip = false
 
 new Vue({
